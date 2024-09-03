6ee0bbbbf6abe5955602ed35b113aa5b7439060c
shivangraikar <shivangraikar@gmail.com>
2024-09-03 14:39:06 -0400

    altered home page styling

--- app/page.js
+++ app/page.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { Typography, Button, Box } from "@mui/material";
+import "../app/styles/styles.css";
+
 
 export default function Home() {
   return (
-    <Box
-      sx={{
-        textAlign: "center",
-        mt: 8,
-      }}
-    >
-      <Typography variant="h2" gutterBottom>
+    <Box className="container">
+      <Typography variant="h1" className="title">
         Welcome to the Tech Marketplace!
       </Typography>
-      <Typography variant="body1" paragraph>
+      <Typography variant="body1" className="subtitle">
         Make your Profile. Connect with others. Explore.
       </Typography>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" className="button">
         Get Started
       </Button>
     </Box>
