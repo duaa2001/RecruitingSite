ea98ceb21c376d2cc623d50fe57ddc8512c6b635
shivangraikar <shivangraikar@gmail.com>
2024-09-03 20:53:19 -0400

    added env variables to .env from firebase

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
-
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAquPRr_xQ925eZL-ga9m7AvBIFyS8--BA",
-  authDomain: "recruitingsite-512b3.firebaseapp.com",
-  projectId: "recruitingsite-512b3",
-  storageBucket: "recruitingsite-512b3.appspot.com",
-  messagingSenderId: "102204045229",
-  appId: "1:102204045229:web:541f092028caf4ed725324"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
